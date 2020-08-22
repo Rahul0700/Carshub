e74bff838a4e00ca2e4bec86ce78edc2108ca8e2
Thejineaswar <thejineash@gmail.com>
2020-08-22 19:20:19 +0530

    db creation

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,40 @@
 from django.db import models
-
+from django.core.validators import MinLengthValidator,MaxLengthValidator
+from django.contrib.auth.models import User
 # Create your models here.
+class inventory(models.Model):
+    car_id = models.CharField(max_length = 6)
+    model = models.CharField(max_length = 20 )
+    built_year = models.PositiveSmallIntegerField()
+    is_petrol = models.BooleanField()
+    is_diesel = models.BooleanField()
+    is_testdrive = models.BooleanField()
+    is_sold = models.BooleanField()
+    price = models.PositiveBigIntegerField()
+
+
+class employee(models.Model):
+    emp_id = models.OneToOneField(User,on_delete=models.CASCADE)
+    name = models.CharField(max_length = 40)
+    contact = models.CharField(max_length=10,validators=[MinLengthValidator(10)])
+    month_sales = models.PositiveBigIntegerField()
+    target = models.PositiveBigIntegerField()
+    commission = models.PositiveBigIntegerField()
+    car_id = models.ForeignKey(inventory, on_delete = models.CASCADE)
+
+class customer(models.Model):
+    cus_id  = models.CharField(max_length = 6)
+    name    = models.CharField(max_length = 50)
+    contact = models.CharField(max_length=10,validators=[MinLengthValidator(10)])
+    address = models.CharField( max_length = 250)
+    emp_id = models.ForeignKey(employee,on_delete = models.CASCADE)
+
+
+class sales(models.Model):
+    sale_id = models.CharField(max_length = 6)
+    date = models.DateField()
+    model = models.CharField(max_length = 10)
+    price_sold = models.PositiveBigIntegerField()
+    car_id = models.ForeignKey(inventory,on_delete = models.CASCADE)
+    emp_id = models.ForeignKey(employee,on_delete = models. CASCADE)
+    cus_id = models.ForeignKey(customer,on_delete = models.CASCADE)
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
 # Create your views here.
+def employeesignup(request):
+    if request.method == 'POST':
+        employee_form = employee_form_create(data=request.POST)
+        employee_profile_form = employeeProfileForm( data = request.POST )
+
+        if employee_form.is_valid() and employee_profile_form.is_valid():
+            info_form = employee_form.save(commit=False)
+            details_form = employee_profile_form.save(commit=False)
+            details_form.name = info_form.username
+            integrity = False
+            while(integrity == False):
+                random_code = randint(1000,9999)
+                code = 'EMP'+ str(random_code)
+                try:
+                    User.objects.get(username=code)
+                    integrity = False
+                except:
+                    integrity = True
+            if integrity:
+                info_form.username = code
+                details_form.employee = info_form
+                info_form.save()
+                details_form.save()
+    else :
+        employee_form = employee_form_create()
+        employee_profile_form = employeeProfileForm()
+    return render(request,'accounts/signup.html',{'form': employee_form,
+                                                  'profile_form':employee_profile_form})
